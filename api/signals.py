bf744d528aa2245ebc082ad4d15fc0009c404e6f
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-18 13:55:38 +0100

    logic to calculating total price

---
+++ api/signals.py
@@ -0,0 +1,13 @@
+# signals.py
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import OrderItem
+
+@receiver([post_save, post_delete], sender=OrderItem)
+def update_order_total(sender, instance, **kwargs):
+    """
+    Signal handler to update the total amount of an Order
+    whenever an OrderItem is created, updated, or deleted.
+    """
+    order = instance.order
+    order.calculate_total()
