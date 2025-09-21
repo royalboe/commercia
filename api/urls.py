f6498d2012b70d76cc6caf6031f2702dfebbd574
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-21 18:42:00 +0100

    adding url for cart

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, CategoryViewSet, ProductViewSet, OrderViewSet
+from .views import UserViewSet, CategoryViewSet, ProductViewSet, OrderViewSet, CartViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'orders', OrderViewSet, basename='order')
+router.register(r'carts', CartViewSet, basename='cart')
 
 urlpatterns = [
     path('', include(router.urls)),
