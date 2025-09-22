09f703b69d869235e4fbb89100e0d876fb443f74
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-22 22:32:17 +0100

    adding authentication

--- commercia/settings.py
+++ commercia/settings.py
@@ -143,3 +143,11 @@ MEDIA_ROOT = BASE_DIR/'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "api.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+}
