15b740708534cd9d485a988958abd15df9ef674a
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-24 21:51:40 +0100

    serializers for wishlist

--- api/serializers/product_serializers.py
+++ api/serializers/product_serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from api.models import Product, Category
+from api.models import Product, Wishlist
 
 class ProductListSerializer(serializers.ModelSerializer):
     """
@@ -66,3 +66,67 @@ class ProductCreateUpdateSerializer(serializers.ModelSerializer):
         if value < 0:
             raise serializers.ValidationError("Price must be a non-negative value.")
         return value
+
+
+class WishlistSerializer(serializers.ModelSerializer):
+    """
+    Read-only serializer for the Wishlist model.
+
+    Purpose:
+        - Used to display wishlist information.
+        - Includes the products in the wishlist.
+
+    Fields:
+        user (CharField, read-only): Username of the user.
+        products (ManyToManyField): Products in the wishlist.
+    """
+    products = ProductListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Wishlist
+        fields = ['wishlist_id', 'user', 'products']
+        read_only_fields = ['wishlist_id']
+
+
+class WishlistCreateUpdateSerializer(serializers.ModelSerializer):
+    """
+    Write serializer for Wishlist.
+
+    Purpose:
+        - Allows creation and update of wishlists with validation.
+
+    Fields:
+        user (CharField): Username of the user.
+        products (List of Primary Keys): List of product IDs to add to the wishlist.
+    """
+    product = serializers.SlugRelatedField(
+        queryset=Product.objects.all(),
+        slug_field='slug',
+        write_only=True
+    )
+
+    class Meta:
+        model = Wishlist
+        fields = ['product']
+
+    def create(self, validated_data: dict):
+        product = validated_data.pop('product')
+        user = validated_data.pop("user")
+        wishlist, created = Wishlist.objects.get_or_create(user=user)
+        if created:
+            wishlist.products.add(product)
+            return wishlist.products.all()
+        if wishlist.products.filter(product_id=product.product_id).exists():
+            wishlist.products.remove(product)
+        else:
+            wishlist.products.add(product)
+        return wishlist.products.all()
+    
+    def update(self, instance, validated_data):
+        product = validated_data.get('product')
+        if instance.products.filter(product_id=product.product_id).exists():
+            instance.products.remove(product)
+        else:
+            instance.products.add(product)
+        return instance
+
