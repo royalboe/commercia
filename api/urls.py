5265dc77e3107dcccd9e590177e88cd39ee0a4bd
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-24 18:08:01 +0100

    urls for reviews

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, CategoryViewSet, ProductViewSet, OrderViewSet, CartViewSet
+from .views import UserViewSet, CategoryViewSet, ProductViewSet, OrderViewSet, CartViewSet, ReviewViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -9,6 +9,7 @@ router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'orders', OrderViewSet, basename='order')
 router.register(r'carts', CartViewSet, basename='cart')
+router.register(r'reviews', ReviewViewSet, basename='review')
 
 urlpatterns = [
     path('', include(router.urls)),
