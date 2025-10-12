9022bcbf97fc22e9d883285610918a2d328f5033
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-10-12 12:52:17 +0100

    REFACTOR: removed users url

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,9 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (CartViewSet, CategoryViewSet, OrderViewSet, ProductViewSet,
-                    ReviewViewSet, UserViewSet, WishlistViewSet)
+                    ReviewViewSet, WishlistViewSet)
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'orders', OrderViewSet, basename='order')
